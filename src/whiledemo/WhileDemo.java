0c09a3a1d2a12b281ae1ea359f636d298c1c9f43
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-08-13 14:41:39 +0700

    Initial commit

---
+++ src/whiledemo/WhileDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whiledemo;
+
+/**
+ *
+ * @author HoangTN
+ */
+public class WhileDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+    
+}
